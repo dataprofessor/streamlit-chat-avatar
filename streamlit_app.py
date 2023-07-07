aa26672330db896edfd22a24bbff845f2ab1e2b6
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 09:59:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with st.chat_message('assistant', avatar='')
+with st.chat_message('assistant', avatar='https://raw.githubusercontent.com/dataprofessor/streamlit-chat-avatar/master/bot-icon.png')
   st.write('Hello world!')
 
 # Icon Credit
