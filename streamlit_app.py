abd9d94ac99513d0459e02b4d11f6f9f0f6505c1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 10:10:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.info('Default')
 with st.chat_message('assistant'):
@@ -16,6 +17,9 @@ st.warning('URL relative path 2')
 with st.chat_message('assistant', avatar='./bot-icon.png'):
   st.write('Hello world!')
 
+st.warning('URL relative path 2')
+with st.chat_message('assistant', avatar=Image.open('bot-icon.png')):
+  st.write('Hello world!')
 
 # Icon Credit
 # https://www.flaticon.com/free-icon/bot_4712027
