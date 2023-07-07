682ba8c03a2d47c9386701921cc78c1321de118c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 10:03:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
+st.info('Default')
 with st.chat_message('assistant', avatar='https://raw.githubusercontent.com/dataprofessor/streamlit-chat-avatar/master/bot-icon.png')
   st.write('Hello world!')
+
+st.warning('URL full path')
+with st.chat_message('assistant', avatar='https://raw.githubusercontent.com/dataprofessor/streamlit-chat-avatar/master/bot-icon.png')
+  st.write('Hello world!')
+
+
 
 # Icon Credit
 # https://www.flaticon.com/free-icon/bot_4712027
