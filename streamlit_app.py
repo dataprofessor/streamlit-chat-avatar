7e0da55f272bc12343de291bf117ede1cbb6b7dc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 10:04:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.info('Default')
-with st.chat_message('assistant', avatar='https://raw.githubusercontent.com/dataprofessor/streamlit-chat-avatar/master/bot-icon.png')
+with st.chat_message('assistant')
   st.write('Hello world!')
 
 st.warning('URL full path')
