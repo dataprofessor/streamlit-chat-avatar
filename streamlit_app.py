0ef6bfd9cdb570a20e52f4815a583e21e684e4d7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 09:55:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+with st.chat_message('assistant', avatar='')
+  st.write('Hello world!')
 
-st.write('Hello world!')
+# Icon Credit
+# https://www.flaticon.com/free-icon/bot_4712027
