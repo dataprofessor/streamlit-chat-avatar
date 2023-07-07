f2f86a40789e1098574ad34fe52f8ac689f12e4c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-07 10:11:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,19 @@ st.info('Default')
 with st.chat_message('assistant'):
   st.write('Hello world!')
 
-st.warning('URL full path')
+st.warning('1. URL full path')
 with st.chat_message('assistant', avatar='https://raw.githubusercontent.com/dataprofessor/streamlit-chat-avatar/master/bot-icon.png'):
   st.write('Hello world!')
 
-st.warning('URL relative path')
+st.warning('2. URL relative path')
 with st.chat_message('assistant', avatar='bot-icon.png'):
   st.write('Hello world!')
 
-st.warning('URL relative path 2')
+st.warning('3. URL relative path 2')
 with st.chat_message('assistant', avatar='./bot-icon.png'):
   st.write('Hello world!')
 
-st.warning('URL relative path 2')
+st.warning('4. URL relative path with Image.open')
 with st.chat_message('assistant', avatar=Image.open('bot-icon.png')):
   st.write('Hello world!')
 
